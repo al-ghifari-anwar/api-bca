d13b0402876570c44a1352614ae145d44d2aa1e1
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-07-12 00:22:29 +0700

    api transfer test

--- snapIntrabankTest.php
+++ snapIntrabankTest.php
@@ -14,7 +14,7 @@ $method = "POST";
 $url = "/openapi/v2.0/transfer-interbank";
 // $getToken = mysqli_query($conn, "SELECT * FROM tb_bca_token WHERE id_bca_token = 1");
 // $rowToken = $getToken->fetch_array(MYSQLI_ASSOC);
-$oauth_token = "Jh04A2862f4HobvBJ8rQczGgRfKSvZ82dhiZH72ZMb8kklSI33wcLv";
+$oauth_token = "K9FHIx7V4Yee1FcFYEuh6fAK06ra8dhdcalBF2J0HRRxtnhKU995sc";
 // For Body
 $partnerReferenceNo = date("YmdHis") . rand(10000000, 99999999);
 $fromDate = date('2024-05-18\T00:00:00P');
