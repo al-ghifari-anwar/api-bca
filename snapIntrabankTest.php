b3ff9f662f1e39d356ea6007f04822ae3bc9c586
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-07-11 23:40:53 +0700

    api transfer test

--- snapIntrabankTest.php
+++ snapIntrabankTest.php
@@ -72,7 +72,7 @@ curl_setopt_array($curl, array(
         // 'Content-Type: application/json',
         'Authorization: Bearer ' . $oauth_token,
         // 'X-EXTERNAL-ID: ' . rand(10000000, 99999999),
-        // 'X-TIMESTAMP: ' . $time_stamp,
+        'X-TIMESTAMP: ' . $time_stamp,
         // 'X-SIGNATURE: ' . $signatureApi
     ),
 ));
