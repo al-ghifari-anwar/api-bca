2daae89c434d733860cf595535c60848d4060753
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-07-22 11:38:32 +0700

    INTERBANK test

--- snapAccountInquiryExternal.php
+++ snapAccountInquiryExternal.php
@@ -14,7 +14,7 @@ $method = "POST";
 $url = "/openapi/v1.0/account-inquiry-external";
 // $getToken = mysqli_query($conn, "SELECT * FROM tb_bca_token WHERE id_bca_token = 1");
 // $rowToken = $getToken->fetch_array(MYSQLI_ASSOC);
-$oauth_token = 'iwGiVzSPtas68uOhw7YgPzOQnpyf2q4tFAdhnos1sL2n7ZbPNKnYCM';
+$oauth_token = 'qzT1c2zACO2vkxTecQnUSsUhwEp9NgS7LkHqBVaWALb4OD01mI6XDq';
 // For Body
 $partnerReferenceNo = date("YmdHis") . rand(10000000, 99999999);
 $fromDate = date('Y-m-d\T00:00:00P');
@@ -55,7 +55,7 @@ curl_setopt_array($curl, array(
     CURLOPT_POSTFIELDS => '{
   "partnerReferenceNo": "' . $partnerReferenceNo . '",
   "beneficiaryAccountNo": "888801000157508",
-  "beneficiaryBankCode": "ABALIDBS",
+  "beneficiaryBankCode": "ABALIDBS"
 }',
     CURLOPT_HTTPHEADER => array(
         'Authorization: Bearer ' . $oauth_token,
