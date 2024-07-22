23219e2964a06c508e9f9131e5d8828122d79a10
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-07-22 11:42:52 +0700

    INTERBANK test

--- snapAccountInquiryExternal.php
+++ snapAccountInquiryExternal.php
@@ -19,7 +19,7 @@ $oauth_token = 'qzT1c2zACO2vkxTecQnUSsUhwEp9NgS7LkHqBVaWALb4OD01mI6XDq';
 $partnerReferenceNo = date("YmdHis") . rand(10000000, 99999999);
 $fromDate = date('Y-m-d\T00:00:00P');
 $toDate = date('Y-m-d\T00:00:00P');
-$bodyStr = "{\"partnerReferenceNo\": \"$partnerReferenceNo\",\"beneficiaryAccountNo\": \"8888010001575081\",\"beneficiaryBankCode\": \"789\"}";
+$bodyStr = "{\"partnerReferenceNo\": \"$partnerReferenceNo\",\"beneficiaryAccountNo\": \"888801000157508\",\"beneficiaryBankCode\": \"789\"}";
 $body = json_decode($bodyStr, true);
 // echo $fromDate;
 
@@ -65,7 +65,7 @@ curl_setopt_array($curl, array(
         'X-SIGNATURE: ' . $signatureApi,
         'ORIGIN: ' . 'topmortarindonesia.com',
         'X-EXTERNAL-ID: ' . rand(10000000, 99999999),
-        'X-PARTNER-ID: UATCORP001',
+        'X-PARTNER-ID: UATCORP0011',
         'X-CLIENT-KEY: ' . $client_id
     ),
 ));
