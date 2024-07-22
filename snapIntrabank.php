7297f1849e1b6dc8f895792b4850fb3af00686ae
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-07-22 09:33:43 +0700

    INTERBANK test

--- snapIntrabank.php
+++ snapIntrabank.php
@@ -19,7 +19,7 @@ $oauth_token = 'r62kdnHgB3Hl6aKSDYquZwYK6OnKIaRyhcApvpQ7YWB9p3dO759Hkf';
 $partnerReferenceNo = date("YmdHis") . rand(10000000, 99999999);
 $fromDate = date('Y-m-d\T00:00:00P');
 $toDate = date('Y-m-d\T00:00:00P');
-$bodyStr = "{\"partnerReferenceNo\": \"$partnerReferenceNo\",\"amount\": {\"value\": \"10000.00\",\"currency\": \"IDR\"},\"beneficiaryAccountNo\": \"0613105953\",\"remark\": \"remark\",\"sourceAccountNo\": \"0611102380\",\"transactionDate\": \"$time_stamp\",\"additionalInfo\": {\"transactionPurpose\": \"01\"}}";
+$bodyStr = "{\"partnerReferenceNo\": \"$partnerReferenceNo\",\"amount\": {\"value\": \"10000.00\",\"currency\": \"IDR\"},\"beneficiaryAccountNo\": \"0613105953\",\"remark\": \"remark\",\"sourceAccountNo\": \"0611102380\",\"transactionDate\": \"$time_stamp\",\"additionalInfo\": {\"economicActivity\": \"Biaya Hidup Pihak Asing 02\",\"transactionPurpose\": \"01\"}}";
 $body = json_decode($bodyStr, true);
 // echo $fromDate;
 
@@ -63,6 +63,7 @@ curl_setopt_array($curl, array(
   "sourceAccountNo": "0611102380",
   "transactionDate": "' . $time_stamp . '",
   "additionalInfo": {
+    "economicActivity": "Biaya Hidup Pihak Asing 02",
     "transactionPurpose": "01"
   }
 }',
