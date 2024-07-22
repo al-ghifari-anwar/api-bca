fc4dfba4229454c42445c8947dec35888141845c
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-07-22 07:40:36 +0700

    INTERBANK test

--- snapAccountInquiryExternal.php
+++ snapAccountInquiryExternal.php
@@ -19,7 +19,7 @@ $oauth_token = 'iwGiVzSPtas68uOhw7YgPzOQnpyf2q4tFAdhnos1sL2n7ZbPNKnYCM';
 $partnerReferenceNo = date("YmdHis") . rand(10000000, 99999999);
 $fromDate = date('Y-m-d\T00:00:00P');
 $toDate = date('Y-m-d\T00:00:00P');
-$bodyStr = "{\"partnerReferenceNo\": \"$partnerReferenceNo\",\"beneficiaryAccountNo\": \"888801000157508\",\"beneficiaryBankCode\": \"BRINDIJA\"}";
+$bodyStr = "{\"partnerReferenceNo\": \"$partnerReferenceNo\",\"beneficiaryAccountNo\": \"888801000157508\",\"beneficiaryBankCode\": \"ABALIDBS\"}";
 $body = json_decode($bodyStr, true);
 // echo $fromDate;
 
@@ -55,7 +55,7 @@ curl_setopt_array($curl, array(
     CURLOPT_POSTFIELDS => '{
   "partnerReferenceNo": "' . $partnerReferenceNo . '",
   "beneficiaryAccountNo": "888801000157508",
-  "beneficiaryBankCode": "BRINDIJA",
+  "beneficiaryBankCode": "ABALIDBS",
 }',
     CURLOPT_HTTPHEADER => array(
         'Authorization: Bearer ' . $oauth_token,
