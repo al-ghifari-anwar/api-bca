5dd49b8f8a2bad866a430d4faa0875915917e085
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-07-22 07:11:29 +0700

    INTERBANK test

--- snapIntrabankTest.php
+++ snapIntrabankTest.php
@@ -19,7 +19,7 @@ $oauth_token = 'hVTip00mU2DSOw3QW39W2exWtRFm49sIc7oqHnEcznlyqDD7YQT1pG';
 $partnerReferenceNo = date("YmdHis") . rand(10000000, 99999999);
 $fromDate = date('Y-m-d\T00:00:00P');
 $toDate = date('Y-m-d\T00:00:00P');
-$bodyStr = "{\"partnerReferenceNo\": \"$partnerReferenceNo\",\"amount\": {\"value\": \"10000.00\",\"currency\": \"IDR\"},\"beneficiaryAccountName\": \"Yories Yolanda\",\"beneficiaryAccountNo\": \"888801000157508\",\"beneficiaryBankCode\": \"BRINIDJAXXX\",\"sourceAccountNo\": \"0611102380\",\"transactionDate\": \"$time_stamp\",\"additionalInfo\": {\"transferType\": \"1\",\"purposeCode\": \"1\"}}";
+$bodyStr = "{\"partnerReferenceNo\": \"$partnerReferenceNo\",\"amount\": {\"value\": \"10000.00\",\"currency\": \"IDR\"},\"beneficiaryAccountName\": \"Yories Yolanda\",\"beneficiaryAccountNo\": \"888801000157508\",\"beneficiaryBankCode\": \"BRINDIJA\",\"sourceAccountNo\": \"0611102380\",\"transactionDate\": \"$time_stamp\",\"additionalInfo\": {\"transferType\": \"1\",\"purposeCode\": \"1\"}}";
 $body = json_decode($bodyStr, true);
 // echo $fromDate;
 
@@ -60,7 +60,7 @@ curl_setopt_array($curl, array(
   },
   "beneficiaryAccountName": "Yories Yolanda",
   "beneficiaryAccountNo": "888801000157508",
-  "beneficiaryBankCode": "BRINIDJAXXX",
+  "beneficiaryBankCode": "BRINDIJA",
   "sourceAccountNo": "0611102380",
   "transactionDate": "' . $time_stamp . '",
   "additionalInfo": {
