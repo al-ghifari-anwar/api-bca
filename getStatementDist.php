e993ad697014acbfd4043091e1e842fea1bccf60
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-12-20 09:10:22 +0700

    auto tf dist

--- getStatementDist.php
+++ getStatementDist.php
@@ -110,6 +110,36 @@ foreach ($detailData as $detailData) {
                         $savePayment = mysqli_query($conn, "INSERT INTO tb_payment(amount_payment,date_payment,remark_payment,id_invoice, source) VALUES($amount,'$transactionDate','$remark',$id_invoice,'8880964519')");
 
                         if ($savePayment) {
+                            // $getContact = mysqli_query($conn, "SELECT * FROM tb_contact JOIN tb_city ON tb_city.id_city = tb_contact.id_city WHERE id_contact = '$id_contact'");
+                            // $rowContact = $getContact->fetch_array(MYSQLI_ASSOC);
+                            $id_distributor = 6;
+
+                            $getCompany = mysqli_query($conn, "SELECT * FROM tb_company WHERE id_distributor = '$id_distributor'");
+                            $rowCompany = $getCompany->fetch_array(MYSQLI_ASSOC);
+
+                            $to_name = $rowCompany['name_company'];
+                            $to_account = $rowCompany['norek_company'];
+
+                            // Send Money
+                            $to_name = str_replace(" ", "%20", $to_name);
+                            // TF intrabank
+                            $curl = curl_init();
+
+                            curl_setopt_array($curl, array(
+                                CURLOPT_URL => 'https://apibca.topmortarindonesia.com/snapIntrabankVctukang.php?to=' . $to_account . '&to_name=' . $to_name,
+                                CURLOPT_RETURNTRANSFER => true,
+                                CURLOPT_ENCODING => '',
+                                CURLOPT_MAXREDIRS => 10,
+                                CURLOPT_TIMEOUT => 0,
+                                CURLOPT_FOLLOWLOCATION => true,
+                                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                                CURLOPT_CUSTOMREQUEST => 'GET',
+                            ));
+
+                            $response = curl_exec($curl);
+
+                            curl_close($curl);
+
                             $return = ["response" => 200, "status" => "ok", "message" => "Invoice paid!"];
                             echo json_encode($return);
                         } else {
@@ -138,6 +168,34 @@ foreach ($detailData as $detailData) {
                 $savePayment = mysqli_query($conn, "INSERT INTO tb_payment(amount_payment,date_payment,remark_payment,id_invoice,source) VALUES($amount,'$transactionDate','$remark',$id_invoice,'8880964519')");
 
                 if ($savePayment) {
+                    $id_distributor = 6;
+
+                    $getCompany = mysqli_query($conn, "SELECT * FROM tb_company WHERE id_distributor = '$id_distributor'");
+                    $rowCompany = $getCompany->fetch_array(MYSQLI_ASSOC);
+
+                    $to_name = $rowCompany['name_company'];
+                    $to_account = $rowCompany['norek_company'];
+
+                    // Send Money
+                    $to_name = str_replace(" ", "%20", $to_name);
+                    // TF intrabank
+                    $curl = curl_init();
+
+                    curl_setopt_array($curl, array(
+                        CURLOPT_URL => 'https://apibca.topmortarindonesia.com/snapIntrabankVctukang.php?to=' . $to_account . '&to_name=' . $to_name,
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => '',
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 0,
+                        CURLOPT_FOLLOWLOCATION => true,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => 'GET',
+                    ));
+
+                    $response = curl_exec($curl);
+
+                    curl_close($curl);
+
                     $return = ["response" => 200, "status" => "ok", "message" => "Payment saved but don't have an invoice data!"];
                     echo json_encode($return);
                 } else {
